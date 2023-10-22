91497eeb35b0e26360e4d47d19efe69273c65c26
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-10-22 18:36:27 +0300

    check

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,7 +4,6 @@
 // import Header from 'components/Header';
 // import Hero from 'components/Hero';
 // import Benefits from 'components/Benefits';
-// import Footer from 'components/Footer';
 
 const Favorites = () => {
   return (
