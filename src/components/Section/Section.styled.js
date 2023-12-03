fb33b523b9485a39cba9600b256d8cd83eb82300
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-12-03 13:43:45 +0200

    add responsiv

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -4,19 +4,23 @@ import { variables } from '../../stylesheet/variables';
 
 export const Section = styled.section`
   padding-bottom: 50px;
+  margin-right: auto;
+  margin-left: auto;
 `;
 
 export const Container = styled.div`
- @media screen and (max-width: ${variables.breakpoints.tablet}) {
-  padding-left: 20px;
-  padding-right: 20px;
-  margin-right: auto;
-  margin-left: auto;
- }
+  @media screen and (max-width: ${variables.breakpoints.tablet}) {
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-right: auto;
+    margin-left: auto;
+  }
 
   @media screen and (min-width: ${variables.breakpoints.desktop}) {
-    width: ${variables.breakpoints.desktop}
+    width: ${variables.breakpoints.desktop};
     padding-left: 29px;
     padding-right: 29px;
+    margin-right: auto;
+    margin-left: auto;
   }
 `;
